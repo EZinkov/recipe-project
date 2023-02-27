6929e72930a8eac868462b65c739277c2b207d2d
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-27 17:25:23 +0100

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <div className="w-full relative bottom-0 left-0 right-0 top-0 my-0 mx-auto text-center py-6 mt-8 bg-gray-200 rounded-t-md">
+      Made by Evgeniy Zinkov ❤️
+    </div>
+  )
+}
+
+export default Footer
